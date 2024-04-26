003731d7348e3b30ffc7bc6195b5d5334b9493f3
Thomas <thomas.nikodem@posteo.de>
2024-04-26 23:33:54 +0200

    cipytest

--- tests/windows/test_window_main.py
+++
@@ -1,7 +0,0 @@
-from mymoneyvisualizer.windows.window_main import WindowMain
-
-
-
-def test_create_account(qtbot, config):
-    window_main = WindowMain(config=config)
-    qtbot.add_widget(window_main)
